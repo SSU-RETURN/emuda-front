88c8c484f12ab1f382e356b04a3cae43ed3c55b3
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-05-08 06:55:43 +0900

    ✨ [Feat] MusicCard UI 작업완료

--- emuda/src/main.jsx
+++ emuda/src/main.jsx
@@ -7,6 +7,7 @@ import PlayListView from './Pages/PlayListView/PlayListView.jsx';
 import MoreRecommendationView from './Pages/RecommendationView/MoreRecommendationView/MoreRecommendationView.jsx';
 import WriteDiaryView from './Pages/WriteDiaryView/WriteDiaryView.jsx';
 // import SearchMusicView from './Pages/SearcjMusicView/SearchMusicView.jsx';
+import MusicCardView from './Pages/MusicCardView/MusicCardView.jsx';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   { path: '/write', element: <WriteDiaryView /> },
+  { path: '/card', element: <MusicCardView /> },
   {
     path: '/recommend',
     element: <RecommendationView isDiaryWritten={true} />,
