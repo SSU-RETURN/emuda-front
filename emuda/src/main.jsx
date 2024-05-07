9ea931aa661b23b610a2f993d7148c958c95a72d
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-05-08 04:47:37 +0900

    ✨ [Feat] SearchMusicView UI 작업완료

--- emuda/src/main.jsx
+++ emuda/src/main.jsx
@@ -6,7 +6,7 @@ import RecommendationView from './Pages/RecommendationView/RecommendationView.js
 import PlayListView from './Pages/PlayListView/PlayListView.jsx';
 import MoreRecommendationView from './Pages/RecommendationView/MoreRecommendationView/MoreRecommendationView.jsx';
 import WriteDiaryView from './Pages/WriteDiaryView/WriteDiaryView.jsx';
-// import SearchMusicView from './Pages/SearcjMusicView/SearchMusicView.jsx';
+import SearchMusicView from './Pages/SearchMusicView/SearchMusicView.jsx';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   { path: '/write', element: <WriteDiaryView /> },
+  { path: '/search', element: <SearchMusicView /> },
   {
     path: '/recommend',
     element: <RecommendationView isDiaryWritten={true} />,
